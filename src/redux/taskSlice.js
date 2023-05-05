773d8c77d66e551cf7779bba1d5cde2e3bdf370c
Dawid Czyzewski <vinciebr@proton.me>
2023-05-05 22:26:08 +0200

    rewrited on builder - redux 2.0

--- src/redux/taskSlice.js
+++ src/redux/taskSlice.js
@@ -1,6 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { tasksInitialState } from "./reducer";
-// import { nanoid } from "nanoid";
 import { addTask, fetchTasks, toggleCompleted } from "./operations";
 
 const handlePending = (state) => {
@@ -12,6 +10,14 @@ const handleRejected = (state, action) => {
   state.error = action.payload;
 };
 
+const isPendingAction = (action) => {
+  return action.type.endsWith("/pending");
+};
+
+const isRejectAction = (action) => {
+  return action.type.endsWith("/rejected");
+};
+
 export const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
@@ -19,78 +25,71 @@ export const tasksSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  // reducers: {
-  //   // this reducer will be started when I initiate backend request
-  //   fetchingInProgress(state, action) {
-  //     state.isLoading = true;
-  //   },
-  //   fetchingSuccess(state, action) {
-  //     // I will use immer:
+  extraReducers: (builder) => {
+    builder
+      // .addCase(fetchTasks.pending, handlePending)
+      // .addCase(addTask.pending, handlePending)
+      // .addCase(toggleCompleted.pending, handlePending)
+
+      // .addCase(fetchTasks.rejected, handleRejected)
+      // .addCase(addTask.rejected, handleRejected)
+      // .addCase(toggleCompleted.rejected, handleRejected)
+
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(addTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(toggleCompleted.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const index = state.items.findIndex(
+          (task) => task.id === action.payload.id
+        );
+        state.items[index].completed = !state.items[index].completed;
+      })
+      // It is new one instead 26, 27 and 28 line:
+      .addMatcher(isPendingAction, handlePending)
+      // It is new one instead 29, 30 and 31 line:
+
+      .addMatcher(isRejectAction, handleRejected)
+
+      // if anybody do something what do nothing, its default func or sth like that. It's better when it will never happens :D
+      .addDefaultCase((state, action) => {
+        state.error = "someone used old function, fix it!";
+      });
+  },
+  // extraReducers: {
+  //   [fetchTasks.pending]: handlePending,
+  //   [addTask.pending]: handlePending,
+  //   [toggleCompleted.pending]: handlePending,
+  //   [fetchTasks.rejected]: handleRejected,
+  //   [addTask.rejected]: handleRejected,
+  //   [toggleCompleted.rejected]: handleRejected,
+  //   [fetchTasks.fulfilled](state, action) {
   //     state.isLoading = false;
   //     state.error = null;
   //     state.items = action.payload;
   //   },
-  //   fetchingError(state, action) {
+  //   [addTask.fulfilled](state, action) {
   //     state.isLoading = false;
-  //     state.error = action.payload;
-  //     // I can also clear items here, but now I don't want.
+  //     state.error = null;
+  //     state.items.push(action.payload);
   //   },
-
-  // addTask: {
-  //   reducer(state, action) {
-  //     state.push(action.payload);
-  //   },
-  //   prepare(text) {
-  //     return {
-  //       payload: { text, id: nanoid(), completed: false },
-  //     };
+  //   [toggleCompleted.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     const index = state.items.findIndex(
+  //       (task) => task.id === action.payload.id
+  //     );
+  //     state.items[index].completed = !state.items[index].completed;
   //   },
   // },
-  // deleteTask(state, action) {
-  //   return state.filter((task) => task.id !== action.payload);
-  // },
-  // toggleCompleted(state, action) {
-  //   for (const task of state) {
-  //     if (task.id === action.payload) {
-  //       task.completed = !task.completed;
-  //     }
-  //   }
-  // },
-
-  // },
-  // extraReducers is for createAsyncThunk
-  extraReducers: {
-    [fetchTasks.pending]: handlePending,
-    [addTask.pending]: handlePending,
-    [toggleCompleted.pending]: handlePending,
-    [fetchTasks.rejected]: handleRejected,
-    [addTask.rejected]: handleRejected,
-    [toggleCompleted.rejected]: handleRejected,
-    [fetchTasks.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [addTask.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(action.payload);
-    },
-    [toggleCompleted.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(
-        (task) => task.id === action.payload.id
-      );
-      state.items[index].completed = !state.items[index].completed;
-    },
-  },
 });
 
-// put changing element and when there is no element for example Tomek, it will add this and PATCH when there is no element won't add this, just leave (change nothing)
-
-// export const { addTask, deleteTask, toggleCompleted } = tasksSlice.actions;
 export const tasksReducer = tasksSlice.reducer;
-
-// export const { fetchingInProgress, fetchingError, fetchingSuccess } =
-//   tasksSlice.actions;
